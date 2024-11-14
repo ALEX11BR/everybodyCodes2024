9ae58e0dfa696b17240b90bab9ea1209f9581144
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2024-11-14 10:30:26 +0200

    d8 (p3 finish - the fixed version of the p3)

--- d8/p3.py
+++ d8/p3.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 
-#TODO
-
 if __name__ == "__main__":
     val = int(input())
 
     columns = [1]
     l = 1
-    ls = [1]
-    lrepeat = -1
     w = 1
     used = 1
-    while used < 202400000000:
+    while used < 202400000:
         w += 2
         l = (l * val) % 10 + 10
-        if lrepeat == -1:
-            if l in ls:
-                lrepeat = ls.index(l)
-            else:
-                ls.append(l)
-        columns = [l] + [x + l for x in columns] + [l]
+        columns = [x + l for x in columns] + [l]
         used += l * w
     for i in range(1, len(columns) - 1):
-        used -= (val * w * columns[i]) % 10
-    ans = (used - 202400000000)
+        used -= ((val * w * columns[i]) % 10) * 2
+    used -= (val * w * columns[0]) % 10
+    ans = (used - 202400000)
     print(ans)
